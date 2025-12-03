e12036e2d084ad8c732625aadc82a9042c2da119
James Stronach <jstronach@bayviewglen.ca>
2025-12-03 13:56:09 -0500

    lemonade stand broken commit 2

--- Unit 4 - Repetition and Iteration/day3/lemonade_stand.py
+++ Unit 4 - Repetition and Iteration/day3/lemonade_stand.py
@@ -18,37 +18,42 @@ def introscreen():
 
 def decisions(money, day):
     print("Day", day)
+    print("Money:", money)
     glass_cost = 2
     sign_cost = 15
     weathers = ["Sunny", "Cloudy", "Hot and Dry"]
     weather = random.choice(weathers)
 
     print("The weather is", weather)
+
     glasses = int(input("How many glasses would you like to make? "))
+
+    while glasses*glass_cost > money:
+        print("You don't have that much money as you only have ",money, "cents")
+        glasses = int(input("How many glasses would you like to make? "))
+
     signs = int(input("How many signs would you like to make? "))
+    money -= signs*sign_cost
 
     glass_price = int(input("How much money would you like to sell your lemonade for (in cents)"))
-    if glass_price > money:
-        print("You don't have that much money as you only have ",money, "cents")
-        #bring us back to asking
-    
+  
     return glasses, signs, glass_price, money, weather
 
 # returns how much money you have left
 def sold(glasses, signs, glass_price, money, weather):
 # need to incorporate signs into the equasion
     if weather == "Sunny" and glass_price <= 18:
-        glasses_sold = (40 - 78)
+        glasses_sold = random.randint(40, 78)*0.01*glasses
     elif weather == "Sunny" and glass_price >= 18:
-        glasses_sold = (40 - 57)
+        glasses_sold = random.randint(40, 57)*0.01*glasses
     elif weather == "Cloudy" and glass_price >= 5:
-        glasses_sold = (10 - 20)
+        glasses_sold = random.randint(10, 20)*0.01*glasses
     elif weather == "Cloudy" and glass_price <= 5:
-        glasses_sold = (20 - 40)
+        glasses_sold = random.randint(20, 40)*0.01*glasses
     elif weather == "Hot and Dry" and glass_price >= 25:
-        glasses_sold = (67 - 80)
+        glasses_sold = random.randint(67, 80)*0.01*glasses
     elif weather == "Hot and Dry" and glass_price <= 25:
-        glasses_sold = #(80 - 100%) of glasses and don't just make it percentage because what if they make like 2 glasses, they're gonna sell out ofc
+        glasses_sold = random.randint(80, 100)*0.01*glasses # and don't just make it percentage because what if they make like 2 glasses, they're gonna sell out ofc
 
     money -= glasses*2
     money -= signs*15
@@ -59,6 +64,3 @@ while gameover == False:
     glasses, signs, glass_price, money, weather = decisions(money, day)
     money = sold(glasses, signs, glass_price, money, weather)
     day+=1
-    print("Day", day)
-
-# print("Money:", money)
